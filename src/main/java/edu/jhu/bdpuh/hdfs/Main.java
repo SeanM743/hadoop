364986a0c0e1f3146880d491619694b78d7b479b
Perry Wilson <pwilso12@jh.edu>
2019-09-14 23:59:23 -0400

    using addResource to add core-site.xml

--- src/main/java/edu/jhu/bdpuh/hdfs/Main.java
+++ src/main/java/edu/jhu/bdpuh/hdfs/Main.java
@@ -1,10 +1,15 @@
 package edu.jhu.bdpuh.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 
 public class Main {
     Configuration conf = new Configuration();
     static final String HelloMessage = "Hello HDFS world!";
+
+    public void addCoreSite() {
+        conf.addResource(new Path("/usr/local/hadoop/hadoop-3.2.0/etc/hadoop/core-site.xml"));
+    }
 
     public String getDefaultFS() { return this.conf.get("fs.defaultFS"); }
 
@@ -14,6 +19,7 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
+        main.addCoreSite();
         System.out.println(main.sayHello());
         System.out.printf("fs.defaultFS: %s\n", main.getDefaultFS());
     }
