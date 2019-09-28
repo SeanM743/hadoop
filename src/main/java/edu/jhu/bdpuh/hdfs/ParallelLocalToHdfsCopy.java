d1d89da974473e3044f8a85f58d9f6368111299a
SeanM743 <sean.mahoney743@gmail.com>
2019-09-27 23:38:36 -0400

    updating with copy and threaded

--- src/main/java/edu/jhu/bdpuh/hdfs/ParallelLocalToHdfsCopy.java
+++ src/main/java/edu/jhu/bdpuh/hdfs/ParallelLocalToHdfsCopy.java
@@ -1,20 +1,13 @@
 package edu.jhu.bdpuh.hdfs;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
 import java.net.URI;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.FileInputStream;
-import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.io.compress.CompressionOutputStream;
-import org.apache.hadoop.io.compress.GzipCodec;
-import org.apache.hadoop.io.compress.CompressionCodec;
-
-//import com.squareup.okhttp.internal.io.FileSystem;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.Arrays;
 
 public class ParallelLocalToHdfsCopy {
 
@@ -26,10 +19,10 @@ public class ParallelLocalToHdfsCopy {
             System.exit(-1);
         }
         // check if directory exists on local file system using standard java library
-        File f = new File(args[0]);
+        File folder = new File(args[0]);
 
-        if (!f.exists()) {
-            System.err.println("File does not exist");
+        if (!folder.exists()) {
+            System.err.println("Folder does not exist");
             System.exit(-1);
         }
 
@@ -38,44 +31,45 @@ public class ParallelLocalToHdfsCopy {
         int nThreads = Integer.parseInt(args[2]);
 
         Path dest = new Path(destPath);
-        Path src = new Path(sourcePath);
 
         Configuration conf = new Configuration();
         conf.addResource(new Path("/usr/local/hadoop/hadoop-3.2.0/etc/hadoop/core-site.xml"));
         conf.addResource(new Path("/usr/local/hadoop/hadoop-3.2.0/etc/hadoop/hdfs-site.xml"));
 
         FileSystem hdfs = FileSystem.get(URI.create(args[1]), conf);
+
+        // if (hdfs.exists(dest)){
+        //     System.err.println("Destination directory already exists! " + 
+        //         "Please delete before running program.");
+        //     System.exit(-1);
+        // }
         if (hdfs.exists(dest)){
-            System.err.println("Destination directory already exists! " + 
-                "Please delete before running program.");
-            System.exit(-1);
+            hdfs.delete(dest, true);
         }
 
         hdfs.mkdirs(dest);
 
-        // FileStatus[] fileStatus = hdfs.listStatus(dest);
-        // for(FileStatus status : fileStatus){
-        //     System.out.println(destPath);
-        //     System.out.println(status.getPath().toString());
-        // }
+        File f = new File(sourcePath);
+        File[] listOfFiles = f.listFiles();
+        int num_files = listOfFiles.length;
+        int files_per_threads = num_files / nThreads;
 
-        try{
-            InputStream is = new BufferedInputStream(new FileInputStream("/home/sean/hd-work/module4-code/pom.xml"));
-            OutputStream os = hdfs.create(new Path("/destination/compressed.gz"));
-            CompressionCodec compCodec = new GzipCodec();
-            CompressionOutputStream compOs = null;
-            compOs = compCodec.createOutputStream(os);
-
-            //IOUtils.copyBytes(is, compOs, conf);
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-        FileSystem fs = FileSystem.get(new Configuration());
-        System.out.println(fs.exists(new Path(sourcePath)));
         System.out.printf("Copying and compressing local files from '%s' to '%s' using %d threads.\n", sourcePath,
                 destPath, nThreads);
 
-        // student will complete the code required to do this...
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(nThreads);
+
+        for (int i = 0; i < nThreads; i++){
+            File[] files_to_copy = Arrays.copyOfRange(listOfFiles, files_per_threads*i, files_per_threads*i + files_per_threads);
+            ThreadedCopy tcpy = new ThreadedCopy(files_to_copy, sourcePath, destPath, conf);
+            executor.execute(tcpy);
+        }
+        executor.shutdown();
 
     }
 }
+
+
+
+
+
---
+++ src/main/java/edu/jhu/bdpuh/hdfs/ThreadedCopy.java
@@ -0,0 +1,65 @@
+package edu.jhu.bdpuh.hdfs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileInputStream;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import java.io.File;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IOUtils;
+import java.net.URI;
+
+public class ThreadedCopy implements Runnable {
+
+    private String source;
+    private String dest;
+    private File[] f;
+    private Configuration config;
+
+    public ThreadedCopy(File[] files_to_copy, String source, String dest, Configuration conf){
+        this.source = source;
+        this.dest = dest;
+        this.f = files_to_copy;
+        this.config = conf;
+    }
+
+    public void run(){
+
+        for (int i = 0; i< f.length; i++){
+            FileSystem hdfs = null;
+            FileInputStream is = null;
+            OutputStream os = null;
+            CompressionCodec compCodec = null;
+            CompressionOutputStream compOs = null;
+
+            try{
+                String fname = f[i].getName();
+                hdfs = FileSystem.get(URI.create(dest), config);
+                is = new FileInputStream(f[i].getAbsolutePath());
+                //InputStream is = new BufferedInputStream(fin);
+                os = hdfs.create(new Path(dest + '/' + fname), true);
+                System.out.println(dest+ '/' + fname + ".gz");
+                compCodec = new GzipCodec();                
+                compOs = compCodec.createOutputStream(os);
+                IOUtils.copyBytes(is, compOs, config);                
+                
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            finally{
+                try{
+                    IOUtils.closeStream(compOs);
+                    is.close();
+                    //hdfs.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("Job ran");
+    }
+}
